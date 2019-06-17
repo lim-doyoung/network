6d7b3b5d13f50c9a26d52cda99cd6929be7318b2
SongHae <thdgo456@gmail.com>
2019-06-18 01:04:56 +0900

    서버 클라이언트 구조 생성

---
+++ client/MainClient.java
@@ -0,0 +1,11 @@
+public class MainClient {
+
+	
+	public static void main(String[] args) {
+
+	
+	}
+
+
+}
+
---
+++ server/MainServer.java
@@ -0,0 +1,8 @@
+public class MainServer{
+	public static void main(String[] args){
+		String msg = "Hello git";
+		System.out.println(msg)//이 부분에서 에러가 난다.
+		
+	}
+
+}
